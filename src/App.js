fc80c4c5df7aa861e7ea1a9b71a1535bc0e3fd19
rachelikisner <racheli9127@gmail.com>
2024-12-25 15:07:24 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
+import { Link, Route, Routes } from 'react-router-dom';
 import Gallery from './lesson2/components/Gallery.js';
 import Weather from './lesson3/components/page.js';
 function App() {
-  return (
-    <>
-      <Weather></ Weather>
-     </> 
+  return(
+  <BrowserRouter>
+  <nav>
+    <Link to="/">HomePage</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Aontact</Link>
+    <Link to="/services">Services</Link>
+  </nav>
+  <Routes>
+     <Route path="/" element={<homePageComponent/>}/>
+     <Route path="/about" element={<aboutComponent/>}/>
+     <Route path="/Contact" element={<ContactComponent/>}/>
+     <Route path="/services" element={<ServicesComponent/>}/>
+  </Routes>
+  </BrowserRouter>
   );
 }
 
